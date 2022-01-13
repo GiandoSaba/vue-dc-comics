9c758d218dda00a35a9b2f1ab82ea1aa22b24e41
GiandoSaba <giandosaba997@gmail.com>
2022-01-13 16:20:22 +0100

    end main - start footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,118 @@
+<template>
+    <footer>
+        <div class="container">
+            <div class="footer-menu">
+                <ul>
+                    DC comics
+                    <li>
+                        <a href="#">Characters</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Movies</a>
+                    </li>
+                    <li>
+                        <a href="#">TV</a>
+                    </li>
+                    <li>
+                        <a href="#">Games</a>
+                    </li>
+                    <li>
+                        <a href="#">Videos</a>
+                    </li>
+                    <li>
+                        <a href="#">News</a>
+                    </li>
+                </ul>
+                <ul>
+                    DC
+                    <li>
+                        <a href="#">Terms Of Use</a>
+                    </li>
+                    <li>
+                        <a href="#">Privacy Policy (New)</a>
+                    </li>
+                    <li>
+                        <a href="#">Ad Choices</a>
+                    </li>
+                    <li>
+                        <a href="#">Advertising</a>
+                    </li>
+                    <li>
+                        <a href="#">Jobs</a>
+                    </li>
+                    <li>
+                        <a href="#">Subscriptions</a>
+                    </li>
+                    <li>
+                        <a href="#">Talent Workshops</a>
+                    </li>
+                    <li>
+                        <a href="#">CPSC Certificates</a>
+                    </li>
+                    <li>
+                        <a href="#">Ratings</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop Help</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact Us</a>
+                    </li>
+                </ul>
+                <ul>
+                    SITES
+                    <li>
+                        <a href="#">DC</a>
+                    </li>
+                    <li>
+                        <a href="#">MAD Magazine</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Kids</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Universe</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Power Visa</a>
+                    </li>
+                </ul>
+                <ul>
+                    SHOP
+                    <li>
+                        <a href="#">Shop DC</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop DC Collectibles</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="logo">
+                <img src="../assets/img/dc-logo-bg.png">
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    footer {
+        background-image: url('../assets/img/footer-bg.jpg');
+        .container {
+            display: flex;
+        }
+    }
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,37 @@
 <template>
     <main>
-        <div class="container">
-            <h1>--> Content goes here </h1>
-        </div>
+        <section class="content-section">
+            <div class="container">
+                <h1>--> Content goes here </h1>
+            </div>
+        </section>
+        <section class="card-section">
+            <div class="container">
+                <ul>
+                    <li>
+                        <img src="../assets/img/buy-comics-digital-comics.png" alt="Digital-Comics">
+                        <a href="#">Digital Comics</a>
+                    </li>
+                    <li>
+                        <img src="../assets/img/buy-comics-merchandise.png" alt="Digital-Comics">
+                        <a href="#">DC Merchandise</a>
+                    </li>
+                    <li>
+                        <img src="../assets/img/buy-comics-subscriptions.png" alt="Digital-Comics">
+                        <a href="#">Subscriptions</a>
+                    </li>
+                    <li>
+                        <img src="../assets/img/buy-comics-shop-locator.png" alt="Digital-Comics">
+                        <a href="#">Comic Shop Locator</a>
+                    </li>
+                    <li>
+                        <img src="../assets/img/buy-dc-power-visa.svg" alt="Digital-Comics">
+                        <a href="#">DC Power Visa</a>
+                    </li>
+
+                </ul>
+            </div>
+        </section>
     </main>
 </template>
 
@@ -18,14 +47,45 @@ export default {
 </script>
 
 <style scoped lang="scss">
-    main {
+    @import "../assets/scss/parzials/_variables";
+    
+    .container {
+        color: white;
+    }
+    .content-section {
         background-color: black;
         .container {
-            color: white;
             padding: 3em;
             h1 {
                 font-size: 1.5em;
             }
         }
+    }
+    .card-section {
+        background-color: $brand-font-color;
+        padding: 4em;
+        ul {
+            list-style: none;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            li {
+                flex-basis: calc(100% / 5);
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                img {
+                    height: 50px;
+                    margin-right: 1em;
+                }
+                a {
+                    text-decoration: none;
+                    text-transform: uppercase;
+                    color: inherit;
+                }
+            }
+
+        }
+
     }
 </style>
