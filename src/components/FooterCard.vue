e9164b6dd26ca6748e84fdb409833b712820d07c
GiandoSaba <giandosaba997@gmail.com>
2022-01-14 16:20:21 +0100

    main_secion

---
+++ src/components/FooterCard.vue
@@ -0,0 +1,75 @@
+<template>
+  <section class="card-section">
+    <div class="container">
+      <ul>
+        <li>
+          <img
+            src="../assets/img/buy-comics-digital-comics.png"
+            alt="Digital-Comics"
+          />
+          <a href="#">Digital Comics</a>
+        </li>
+        <li>
+          <img
+            src="../assets/img/buy-comics-merchandise.png"
+            alt="Digital-Comics"
+          />
+          <a href="#">DC Merchandise</a>
+        </li>
+        <li>
+          <img
+            src="../assets/img/buy-comics-subscriptions.png"
+            alt="Digital-Comics"
+          />
+          <a href="#">Subscriptions</a>
+        </li>
+        <li>
+          <img
+            src="../assets/img/buy-comics-shop-locator.png"
+            alt="Digital-Comics"
+          />
+          <a href="#">Comic Shop Locator</a>
+        </li>
+        <li>
+          <img src="../assets/img/buy-dc-power-visa.svg" alt="Digital-Comics" />
+          <a href="#">DC Power Visa</a>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "FooterCard",
+};
+</script>
+
+<style scoped lang="scss">
+@import "../assets/scss/parzials/_variables";
+
+.card-section {
+  background-color: $brand-font-color;
+  padding: 2.5em;
+  ul {
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+    li {
+      flex-basis: calc(100% / 5);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      img {
+        height: 50px;
+        margin-right: 1em;
+      }
+      a {
+        text-transform: uppercase;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/FooterSocial.vue
@@ -0,0 +1,74 @@
+<template>
+  <section class="footer-bot">
+    <div class="container">
+      <div class="sign-up">
+        <a href="#">SIGN-UP NOW!</a>
+      </div>
+      <div class="social-link">
+        <ul>
+          <li>
+            <a href="#">Follow Us</a>
+          </li>
+          <li>
+            <img src="../assets/img/footer-facebook.png" alt="" />
+          </li>
+          <li>
+            <img src="../assets/img/footer-periscope.png" alt="" />
+          </li>
+          <li>
+            <img src="../assets/img/footer-pinterest.png" alt="" />
+          </li>
+          <li>
+            <img src="../assets/img/footer-twitter.png" alt="" />
+          </li>
+          <li>
+            <img src="../assets/img/footer-youtube.png" alt="" />
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "FooterSocial",
+};
+</script>
+
+<style scoped lang="scss">
+@import "../assets/scss/parzials/_variables";
+
+.footer-bot {
+  background-color: #303030;
+  padding: 2em 0;
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .sign-up {
+      width: 50%;
+      a {
+        padding: 0.8em;
+        border: 2px solid $brand-font-color;
+        color: white;
+      }
+    }
+    .social-link {
+      width: 50%;
+      ul {
+        list-style: none;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        gap: 2em;
+        a {
+          text-transform: uppercase;
+          font-size: 1.5em;
+          color: $brand-font-color;
+        }
+      }
+    }
+  }
+}
+</style>
